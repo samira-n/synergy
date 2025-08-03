fe4e4d2689bc4990cf34a66ed753735e570bf2cf
samira <s.nabievaaa@yandex.ru>
2025-08-03 14:46:31 +0300

    add files

---
+++ Lesson_3_synergy/task_1.py
@@ -0,0 +1,5 @@
+pet_type = input("Введите вид питомца: ")
+pet_age = input("Введите возраст питомца: ")
+pet_name = input("Введите имя питомца: ")
+
+print("Это", pet_type, "по кличке", pet_name, "Возраст:", pet_age, "года.")
---
+++ Lesson_3_synergy/task_2.py
@@ -0,0 +1,9 @@
+print("Введите этапы развития человека по порядку")
+
+stage1 = input("Первый этап: ")
+stage2 = input("Второй этап: ")
+stage3 = input("Третий этап: ")
+stage4 = input("Четвертый этап: ")
+stage5 = input("Пятый этап: ")
+
+print(stage1, stage2, stage3, stage4, stage5, sep="=>")
